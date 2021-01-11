26eeb235712e70c47f8233901469bd549ae1aa17
Christo-IV <christo.vahtramae@khk.ee>
2021-01-11 09:27:20 +0200

    Exercise 1.7

--- src/index.js
+++ src/index.js
@@ -1,69 +1,55 @@
-// ---------------//
-//  Ex 1.4 - 1.5  //
-// ---------------//
+//----------------//
+//     Ex 1.7     //
+//----------------//
 
-
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Header = (props) => {
-  return (
-    <h1>{props.course}</h1>
-  )
-}
-
-const Part = (props) => {
-  return (
-    <p>
-      {props.part.name}: {props.part.exercises}
-    </p>
-  )
-}
-
-const Content = (props) => {
-  console.log("Content:");
-  console.log(props);
-  return (
-    <div>
-      <Part part={props.parts[0]}/>
-      <Part part={props.parts[1]}/>
-      <Part part={props.parts[2]}/>
-    </div>
-  )
-}
-
-const Total = (props) => {
-  return (
-    <p>Number of exercises: {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
-  )
-}
-
-const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
+const Button = ({text, type, setType}) => {
+  
+  const handleClick = () => {
+    setType(type + 1);
   }
 
   return (
+    <button onClick={handleClick}>
+      {text}
+    </button>
+  )
+}
+
+// using <br> here to make sure that all changes I make to the code are only in this file
+const Stat = ({text, value}) => <>{text}: {value} <br></br></>
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  
+  const total = good + neutral + bad;
+
+  return (
     <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      <section>
+        <h1>Give Feedback</h1>
+        <Button text="good" type={good} setType={setGood}/>
+        <Button text="neutral" type={neutral} setType={setNeutral}/>
+        <Button text="bad" type={bad} setType={setBad}/>
+      </section>
+      <section>
+        <h2>Statistics</h2>
+        <Stat text="Good" value={good} />
+        <Stat text="Neutral" value={neutral} />
+        <Stat text="Bad" value={bad} />
+        <Stat text="All" value={total} />
+        <Stat text="Average" value={(good - bad) / total} />
+        <Stat text="Positive" value={good / total} />
+      </section>
     </div>
   )
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, 
+  document.getElementById('root')
+)
